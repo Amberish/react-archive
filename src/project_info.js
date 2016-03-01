62302d4e8720fdaf19ddba6595bdf8d354efdc08
Amberish Raj <Amberish Raj>
2016-03-01 22:38:52 +0530

    project image added

--- src/project_info.js
+++ src/project_info.js
@@ -6,7 +6,7 @@ export default [
   {
     id: 1,
     name: "Video Browser",
-    image: "image/image1.jpg",
+    image: "image/image1.png",
     content: "In this example you will learn to create a small app like youtube, utilizing youtube API integration.",
     demo_link: "/video-browser"
     //truncLen: 120
@@ -14,19 +14,19 @@ export default [
   {
     id: 2,
     name: "Library App",
-    image: "image/image1.jpg",
+    image: "image/image1.png",
     content: "In this example you will learn to create a library which display books listing and view content of selected book."
   },
   {
     id: 3,
     name: "Blog App",
-    image: "image/image1.jpg",
+    image: "image/image1.png",
     content: "In this example you will learn to create a blog app. In this app user can create, view, and delete posts."
   },
   {
     id: 4,
     name: "Weather App",
-    image: "image/image1.jpg",
+    image: "image/image1.png",
     content: "In this example you will learn to create an app displaying weather information of city. Chart API for displaying trend."
   }
 ];
