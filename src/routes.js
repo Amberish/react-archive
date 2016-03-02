e55eb6ccb3d8ae2855ac794ef8282cacacdb1dfe
Amberish Raj <Amberish Raj>
2016-03-02 15:35:00 +0530

    Route for library app created

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import App from "./components/app";
 import Index from "./components/index";
 import AboutMe from "./components/about_me";
 import VideoBrowser from "../projects/video-browser";
+import LibraryApp from "../projects/library-app";
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={Index} />
     <Route path="about-me" component={AboutMe} />
     <Route path="video-browser" component={VideoBrowser} />
+    <Route path="library-app" component={LibraryApp} />
   </Route>
 );
